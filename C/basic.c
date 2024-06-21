6f22ae3cfb03d668bde78540b81f3edfbd8fbc23
Ayush Yadav <sidharth197502@gmail.com>
2024-06-21 23:29:50 +0530

    Add files via upload

---
+++ C/basic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+struct basic
+{
+    /* data */
+};
+
+
+int main()
+{
+    // int stack=[];
+    int choice;
+
+    printf("1.Push \n2.Pop \n3.Peak \n4.Display \n5.Exit\nEnter your choice:");
+    scanf("%c",&choice);
+
+    if(choice==1)
+    {
+        Push();
+    }
+
+   
+}
