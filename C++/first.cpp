76684e4036f26d8935da83bad0c32059539abb75
Ayush Yadav <sidharth197502@gmail.com>
2024-08-01 23:02:17 +0530

    Created first.cpp

---
+++ C++/first.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // io-> input output, stream --> flow source
+
+using namespace std; // it is a directive which tells the compiler to consider all the symbols in the std namespace as if they are declared in the global namespace.
+
+int main()
+{
+    cout << "Hello World";// used to print 
+
+    return 0;
+}
