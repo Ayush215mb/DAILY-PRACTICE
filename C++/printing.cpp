c9d4acae74c5a0081011632c6153313e152dacab
Ayush Yadav <sidharth197502@gmail.com>
2024-08-01 23:03:02 +0530

    Created printing.cpp

---
+++ C++/printing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello\n"; // cout(seeout) is used to print strings
+
+    int age; // declaring a integer variable
+
+    age = 19; // assigning value to the variable
+
+    cout << age; // printing the integer
+
+    return 0;
+}
