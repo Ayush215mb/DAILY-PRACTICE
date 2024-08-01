df508a6ec55206d07234039e4d0a4c4dbaae3619
Ayush Yadav <sidharth197502@gmail.com>
2024-08-01 23:04:23 +0530

    Create Escape_Characters.cpp

---
+++ C++/Escape_Characters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "some of the Escape characters\n";
+    cout << "to get new line \n";
+    cout << "to get double quote \"   \n";
+    cout << "to get horizontal tab \t like this   \n";
+    cout << "to get backslash \\";
+
+    return 0;
+}
